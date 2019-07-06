ecb268c6646557e26a9bd8c864cf98e082d252ad
Adam Poit <adam.poit@logos.com>
2019-07-06 08:51:46 -0700

    Add grafana.

--- monitoring-setup.sh
+++ monitoring-setup.sh
@@ -5,3 +5,4 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/v0
 kubectl create -f https://github.com/rook/rook/raw/release-1.0/cluster/examples/kubernetes/ceph/monitoring/service-monitor.yaml
 kubectl create -f https://github.com/rook/rook/raw/release-1.0/cluster/examples/kubernetes/ceph/monitoring/prometheus.yaml
 kubectl create -f cluster/rook/monitoring/prometheus-service.yaml
+kubectl create -f cluster/rook/monitoring/grafana.yaml
